93bc761cfb0d804a500a9492825ec81c336d8822
Adrian Draghici <draghici.adrian.b@gmail.com>
2016-05-17 21:54:43 +0100

    Revert "[Plugin]: Adding jclouds library dependencies for the AWS environment."
    This reverts commit c971ad71f27dd772576c01c6ac534d3ca5a74d0a.

--- openmole/project/src/main/scala/root/plugin/Environment.scala
+++ openmole/project/src/main/scala/root/plugin/Environment.scala
@@ -30,7 +30,7 @@ object Environment extends PluginDefaults {
 
   lazy val sge = OsgiProject("sge", imports = Seq("*")) dependsOn (Core.dsl, Core.batch, gridscale, ssh) settings
     (libraryDependencies += Libraries.gridscaleSGE)
-
+  
   lazy val aws = OsgiProject("aws", imports = Seq("!org.jclouds.compute.*", "!org.jclouds.compute", "*")) dependsOn (Core.dsl, Core.batch, gridscale, ssh) settings
     (libraryDependencies += Libraries.gridscaleAWS)
 
